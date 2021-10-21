703965872ec7967741d78bac7ddfcfae31c1098f
Jon McManus <due@wxwhatever.com>
2021-10-22 09:36:24 +1000

    Start work on Minecolonies compat (#6).

--- src/main/java/noobanidus/mods/miniatures/MiniTags.java
+++ src/main/java/noobanidus/mods/miniatures/MiniTags.java
@@ -1,11 +1,29 @@
 package noobanidus.mods.miniatures;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.EntityType;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
 
 public class MiniTags {
+  public static class Entity extends MiniTags {
+    public static Tags.IOptionalNamedTag<EntityType<?>> MOB_ATTACK_BLACKLIST = compatTag("minecolonies", "mob_attack_blacklist");
+
+    static Tags.IOptionalNamedTag<EntityType<?>> modTag(String name) {
+      return EntityTypeTags.createOptional(new ResourceLocation(Miniatures.MODID, name));
+    }
+
+    static Tags.IOptionalNamedTag<EntityType<?>> compatTag(String name) {
+      return compatTag("forge", name);
+    }
+
+    static Tags.IOptionalNamedTag<EntityType<?>> compatTag(String namespace, String name) {
+      return EntityTypeTags.createOptional(new ResourceLocation(namespace, name));
+    }
+  }
+
   public static class Blocks extends MiniTags {
     public static Tags.IOptionalNamedTag<Block> BREAK_BLOCKS = modTag("break_blocks");
 
