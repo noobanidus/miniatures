b27f529b386dd73723c61a9e7147bc9e24df4db4
Jon <due@wxwhatever.com>
2025-02-03 18:51:20 +1000

    Properly memoize stuff.

--- common/src/main/java/noobanidus/mods/miniatures/common/client/AdditionalRenderTypes.java
+++ common/src/main/java/noobanidus/mods/miniatures/common/client/AdditionalRenderTypes.java
@@ -2,9 +2,12 @@ package noobanidus.mods.miniatures.common.client;
 
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+
+import java.util.function.Function;
 
 // TODO: CACHE THESE IMPORTANT
 public class AdditionalRenderTypes extends RenderType {
@@ -12,15 +15,24 @@ public class AdditionalRenderTypes extends RenderType {
     super(p_i225992_1_, p_i225992_2_, p_i225992_3_, p_i225992_4_, p_i225992_5_, p_i225992_6_, p_i225992_7_, p_i225992_8_);
   }
 
-/*  public static RenderType getFullbrightLayer(ResourceLocation pLocation, float pU, float pV) {
-    return create("fullbright_entity", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, CompositeState.builder().setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_SHADER).setTextureState(new TextureStateShard(pLocation, false, false)).setTexturingState(new OffsetTexturingStateShard(pU, pV)).setCullState(NO_CULL).setLightmapState(LIGHTMAP).setTransparencyState(RenderStateShard.NO_TRANSPARENCY).setOverlayState(NO_OVERLAY).createCompositeState(false));
-  }*/
+  private static Function<ResourceLocation, RenderType> GLOWING = Util.memoize((pLocation) -> create("spirit_entity", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, CompositeState.builder()
+      .setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_SHADER)
+      .setTextureState(new TextureStateShard(pLocation, false, false)).setCullState(NO_CULL).setLightmapState(LIGHTMAP)
+      .setTransparencyState(RenderStateShard.GLINT_TRANSPARENCY).setOverlayState(NO_OVERLAY)
+      .createCompositeState(false)));
 
-  public static RenderType getGlowing(ResourceLocation pLocation) {
-    return create("spirit_entity", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, CompositeState.builder().setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_SHADER).setTextureState(new TextureStateShard(pLocation, false, false)).setCullState(NO_CULL).setLightmapState(LIGHTMAP).setTransparencyState(RenderStateShard.GLINT_TRANSPARENCY).setOverlayState(NO_OVERLAY).createCompositeState(false));
+  public static RenderType getGlowing (ResourceLocation location) {
+    return GLOWING.apply(location);
+
   }
 
-  public static RenderType getLightning(ResourceLocation pLocation) {
-    return create("spirit_entity", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, CompositeState.builder().setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_SHADER).setTextureState(new TextureStateShard(pLocation, false, false)).setCullState(NO_CULL).setLightmapState(LIGHTMAP).setTransparencyState(RenderStateShard.LIGHTNING_TRANSPARENCY).setOverlayState(NO_OVERLAY).createCompositeState(false));
+  private static Function<ResourceLocation, RenderType> OTHER_GLOWING = Util.memoize((pLocation) -> create("spirit_entity", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, CompositeState.builder()
+      .setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_SHADER)
+      .setTextureState(new TextureStateShard(pLocation, false, false)).setCullState(NO_CULL).setLightmapState(LIGHTMAP)
+      .setTransparencyState(RenderStateShard.LIGHTNING_TRANSPARENCY).setOverlayState(NO_OVERLAY)
+      .createCompositeState(false)));
+
+  public static RenderType getOtherGlowing (ResourceLocation location) {
+    return OTHER_GLOWING.apply(location);
   }
 }
--- common/src/main/java/noobanidus/mods/miniatures/common/client/model/GhostlyMiniMeModel.java
+++ common/src/main/java/noobanidus/mods/miniatures/common/client/model/GhostlyMiniMeModel.java
@@ -6,6 +6,6 @@ import noobanidus.mods.miniatures.common.entity.MiniMeEntity;
 
 public class GhostlyMiniMeModel<E extends MiniMeEntity> extends MiniMeModel<E> {
   public GhostlyMiniMeModel(ModelPart root, boolean slim) {
-    super(AdditionalRenderTypes::getLightning, root, slim);
+    super(AdditionalRenderTypes::getOtherGlowing, root, slim);
   }
 }
