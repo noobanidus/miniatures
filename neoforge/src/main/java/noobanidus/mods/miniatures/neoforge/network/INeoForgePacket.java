33b871449b670031021cddd494a2f0c5dc73da34
Jon <due@wxwhatever.com>
2026-04-30 15:40:12 +1000

    Start 1.21.5 port.

--- neoforge/src/main/java/noobanidus/mods/miniatures/neoforge/network/INeoForgePacket.java
+++
@@ -1,8 +0,0 @@
-package noobanidus.mods.miniatures.neoforge.network;
-
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.neoforged.neoforge.network.handling.IPayloadContext;
-
-public interface INeoForgePacket extends CustomPacketPayload {
-void handle (IPayloadContext context);
-}
