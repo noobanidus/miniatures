6e2ed82e7013ed6b5a17aa9a8d94d501d10bba15
Jon <due@wxwhatever.com>
2026-05-03 11:46:40 +1000

    More shenanigans to get models rendereing.

--- common/src/main/java/noobanidus/mods/miniatures/common/client/renderer/entity/MiniMeRenderer.java
+++ common/src/main/java/noobanidus/mods/miniatures/common/client/renderer/entity/MiniMeRenderer.java
@@ -7,14 +7,24 @@ import net.minecraft.client.model.HumanoidArmorModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.CustomHeadLayer;
 import net.minecraft.client.renderer.entity.layers.PlayerItemInHandLayer;
 import net.minecraft.client.renderer.entity.layers.WingsLayer;
+import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.client.resources.PlayerSkin;
-import net.minecraft.client.resources.SkinManager;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.numbers.StyledFormat;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.component.ResolvableProfile;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.scores.DisplaySlot;
+import net.minecraft.world.scores.Objective;
+import net.minecraft.world.scores.ReadOnlyScoreInfo;
+import net.minecraft.world.scores.Scoreboard;
 import noobanidus.mods.miniatures.common.client.ModelHolder;
 import noobanidus.mods.miniatures.common.client.model.MiniMeModel;
 import noobanidus.mods.miniatures.common.client.renderer.layers.ArrowRenderTypeLayer;
@@ -68,15 +78,6 @@ public class MiniMeRenderer extends LivingEntityRenderer<MiniMeEntity, MiniRende
     return entity.skin.texture();
   }
 
-  public static ResourceLocation getSkin(ResolvableProfile resolvableProfile) {
-    SkinManager skinmanager = Minecraft.getInstance().getSkinManager();
-    if (resolvableProfile != null) {
-      return skinmanager.getInsecureSkin(resolvableProfile.gameProfile()).texture();
-    } else {
-      return TEXTURE_STEVE;
-    }
-  }
-
   @Override
   public void render(MiniRenderState miniMeEntity, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightIn) {
     this.model = ModelHolder.miniMe;
@@ -110,10 +111,9 @@ public class MiniMeRenderer extends LivingEntityRenderer<MiniMeEntity, MiniRende
 
   protected void setupRotations(MiniRenderState miniMeEntity, PoseStack poseStack, float f, float g) {
     super.setupRotations(miniMeEntity, poseStack, f, g);
-    // TODO: Move this into the extract state
     int noob = miniMeEntity.noobVariant;
     if (noob == 0) {
-      poseStack.translate(0.0D, miniMeEntity.bbHeight, 0.0D);
+      /*      poseStack.translate(0.0D, miniMeEntity.bbHeight, 0.0D);*/
       poseStack.mulPose(Axis.ZP.rotationDegrees(180.0F));
     } else if (noob == 1) {
       poseStack.translate(0.0D, 0.35F, 0.0D);
@@ -124,5 +124,61 @@ public class MiniMeRenderer extends LivingEntityRenderer<MiniMeEntity, MiniRende
     } else if (noob == 8) {
       poseStack.mulPose(Axis.YP.rotationDegrees(180.0f));
     }
+    // TODO: Copy player rendering stuff
+  }
+
+  @Override
+  public void extractRenderState(MiniMeEntity entity, MiniRenderState state, float someFloatValue) {
+    super.extractRenderState(entity, state, someFloatValue);
+    HumanoidMobRenderer.extractHumanoidRenderState(entity, state, someFloatValue, this.itemModelResolver);
+/*        state.leftArmPose = PlayerRenderer.getArmPose(entity, HumanoidArm.LEFT);
+        state.rightArmPose = PlayerRenderer.getArmPose(entity, HumanoidArm.RIGHT);*/
+    state.skin = getSkin(entity); //entity.getSkin();
+    state.arrowCount = entity.getArrowCount();
+    state.stingerCount = entity.getStingerCount();
+    state.useItemRemainingTicks = entity.getUseItemRemainingTicks();
+    state.swinging = entity.swinging;
+    state.isSpectator = entity.isSpectator();
+    state.showHat = true; //entity.isModelPartShown(PlayerModelPart.HAT);
+    state.showJacket = true; //entity.isModelPartShown(PlayerModelPart.JACKET);
+    state.showLeftPants = true; //entity.isModelPartShown(PlayerModelPart.LEFT_PANTS_LEG);
+    state.showRightPants = true; //entity.isModelPartShown(PlayerModelPart.RIGHT_PANTS_LEG);
+    state.showLeftSleeve = true; //entity.isModelPartShown(PlayerModelPart.LEFT_SLEEVE);
+    state.showRightSleeve = true; //entity.isModelPartShown(PlayerModelPart.RIGHT_SLEEVE);
+    state.showCape = true; //entity.isModelPartShown(PlayerModelPart.CAPE);
+/*    PlayerRenderer.extractFlightData(entity, state, entity);
+    PlayerRenderer.extractCapeState(entity, state, entity);*/
+    if (state.distanceToCameraSq < 100.0) {
+      Scoreboard scoreboard = entity.level().getScoreboard();
+      Objective objective = scoreboard.getDisplayObjective(DisplaySlot.BELOW_NAME);
+      if (objective != null) {
+        ReadOnlyScoreInfo readonlyscoreinfo = scoreboard.getPlayerScoreInfo(entity, objective);
+        Component component = ReadOnlyScoreInfo.safeFormatValue(readonlyscoreinfo, objective.numberFormatOrDefault(StyledFormat.NO_STYLE));
+        state.scoreText = Component.empty().append(component).append(CommonComponents.SPACE)
+            .append(objective.getDisplayName());
+      } else {
+        state.scoreText = null;
+      }
+    } else {
+      state.scoreText = null;
+    }
+
+    state.parrotOnLeftShoulder = null; /*getParrotOnShoulder(entity, true);*/
+    state.parrotOnRightShoulder = null; //getParrotOnShoulder(entity, false);
+    state.id = entity.getId();
+/*        state.name = entity.getGameProfile().ifPresent(o ->
+            if (o.)).flatMap(GameProfile::getName).orElse("Minime");*/
+    state.heldOnHead.clear();
+    if (state.isUsingItem) {
+      ItemStack itemstack = entity.getItemInHand(state.useItemHand);
+      if (itemstack.is(Items.SPYGLASS)) {
+        this.itemModelResolver.updateForLiving(state.heldOnHead, itemstack, ItemDisplayContext.HEAD, entity);
+      }
+    }
+  }
+
+  public static PlayerSkin getSkin(MiniMeEntity entity) {
+    return entity.getGameProfile().map(resolvableProfile -> Minecraft.getInstance().getSkinManager()
+        .getInsecureSkin(resolvableProfile.gameProfile())).orElseGet(DefaultPlayerSkin::getDefaultSkin);
   }
 }
--- common/src/main/java/noobanidus/mods/miniatures/common/client/renderer/state/MiniRenderState.java
+++ common/src/main/java/noobanidus/mods/miniatures/common/client/renderer/state/MiniRenderState.java
@@ -2,11 +2,9 @@ package noobanidus.mods.miniatures.common.client.renderer.state;
 
 import net.minecraft.client.renderer.entity.state.PlayerRenderState;
 
-import java.util.UUID;
-
 public class MiniRenderState extends PlayerRenderState {
   // TODO: Noob variants
-  public boolean isPowered;
-  public int noobVariant;
+  public boolean isPowered = false;
+  public int noobVariant = -1;
   public double bbHeight;
 }
